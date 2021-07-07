c1288bc5bc030d9d7cb056f0184ea9cc7178d2fc
giovanna-crypto <giovannalopez826@gmail.com>
2021-07-06 13:34:42 -1100

    modifique los cards en css y el tamaño de los posters, borre algunos datos de html, puse console los en js main para verificar q esten enlazados los datos de ghibli.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data.films);
+const film=`<article id='cd3d059c-09f4-4ff3-8d63-bc765a5184fa'>
+<h2>${data.films[0].title}</h2>
+<div class='pelicula1'>
+  <img src='https://static.wikia.nocookie.net/studio-ghibli/images/0/08/Howl%27s_Moving_Castle.jpg'>
+</div>
+</article>`
+console.log(film);
